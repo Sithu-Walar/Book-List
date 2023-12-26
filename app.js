1c3de11f89dab7212a2b084a546c38710d599aa9
sithu-nanda <sithuyannainglwm@gmail.com>
2023-12-26 23:13:22 +0630

    second commit

--- app.js
+++ app.js
@@ -21,23 +21,60 @@ class UI {
         }
         ]
         const books = storedBook;
-        books.map((book)=>{
-            const list = document.getElementById('book-list')
-            const row = document.createElement('tr')
-
-            row.innerHTML = `
-            <td>${book.name}</td>
-            <td>${book.author}</td>
-            <td>${book.ibsn}</td>
-            <td><a href="" class="btn btn-danger btn-sm delete ">X</a></td>
-            `
-
-            list.appendChild(row)
-        
-        })
+        books.map(book=> UI.addBookToList(book))
 
 
     }
+
+    static addBookToList(book){
+        const list = document.getElementById('book-list')
+        const row = document.createElement('tr')
+
+        row.innerHTML = `
+        <td>${book.name}</td>
+        <td>${book.author}</td>
+        <td>${book.ibsn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete ">X</a></td>
+        `
+
+        list.appendChild(row)
+    
+    }
+    static clearInput(){
+    document.getElementById('name').value = '';
+   document.getElementById('author').value = '';
+    document.getElementById('ibsn').value = '';
+
+    }
+    static deleteBook(el){
+        if(el.classList.contains('delete')){
+            el.parentElement.parentElement.remove()
+        }
+    }
+    
 }
 
 document.addEventListener('DOMContentLoaded',UI.displayStoredBook)
+
+
+document.querySelector('#book-form').addEventListener('submit', e =>{
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const author = document.getElementById('author').value;
+    const ibsn = document.getElementById('ibsn').value;
+
+    if(name == ''|| author ==''|| ibsn == ''){
+        alert('fill in all fields')
+    }else {
+        const book = new Book(name,author,ibsn);
+        console.log(book)
+        UI.addBookToList(book)
+    }
+    
+    UI.clearInput();
+})
+
+document.querySelector('#book-list').addEventListener('click', e => {
+    console.log(e.target)
+    UI.deleteBook(e.target)
+})
